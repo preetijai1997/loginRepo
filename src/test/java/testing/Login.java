2fa80951879ae28cc2d98fa5cda3f9ab2907c8b6
Preeti <preetijaiswal504@gmail.com>
2023-03-29 16:05:23 +0530

    Login Functionality

---
+++ src/test/java/testing/Login.java
@@ -0,0 +1,31 @@
+package testing;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Login {
+
+	public static void main(String[] args) {
+	
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\bs0452\\Downloads\\browsers\\chromedriver.exe");
+		
+		ChromeOptions option= new ChromeOptions();
+		
+		option.addArguments("--remote-allow-origins=*");
+	   WebDriver driver= new ChromeDriver(option);
+	   
+	   driver.get("https://portal.bankit.in:9090/RO/");
+	   driver.manage().window().maximize();
+	   driver.switchTo().frame("Main");
+	   
+	   driver.findElement(By.name("userName")).sendKeys("8957181611");
+	   driver.findElement(By.id("password")).sendKeys("119705");
+	   driver.findElement(By.id("login")).click();
+		
+
+	}
+
+}
